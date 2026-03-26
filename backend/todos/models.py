254b320d8fdd034e47aff23cb120942e1bf2b422
miaQian <53013893+miaqian@users.noreply.github.com>
2026-03-25 22:11:51 -0400

    add date field to Todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -5,6 +5,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    data = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title 
